c6c95d34ff7961f4f807c77cbbe88172053f1e3a
Tayyebwazir <168566182+Tayyebwazir@users.noreply.github.com>
2024-08-28 15:42:38 +0500

    Add files via upload

---
+++ Add_Even_No.py
@@ -0,0 +1,11 @@
+# # Task no:1
+# # whether a number is Even or Odd
+
+# num = int(input("Enter a number: "))
+# if (num % 2) == 0:
+#     print("{0} is Even Number".format(num))
+# else:
+#     print("{0} is Odd Number".format(num))
+
+
+
